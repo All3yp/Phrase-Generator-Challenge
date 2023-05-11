b1ffea0605d3b830378d5d426af428d87253c5a4
Alley Pereira <noyallepereira@gmail.com>
2023-05-10 21:04:41 -0300

    wip: creating menu for get input

---
+++ PhraseGenerator/Phrases.swift
@@ -0,0 +1,20 @@
+//
+//  Phrases.swift
+//  PhraseGenerator
+//
+//  Created by userext on 09/05/23.
+//
+
+import Foundation
+/*
+ Brainstorming de ideias 💡
+
+Criar um txtzin para armazenar as frases. ✅
+Criar um txtzin para guardar as frases (Na hora que ele ler ele escreve)
+Criar uma func p ler o arquivo de texto e armazenar as frases em uma matriz;
+Criar uma func p gerar uma frase aleatória a partir da matriz de frases armazenadas;
+Criar uma opção de menu (switchzin de menu classico) para permitir que o usuário peça uma nova frase aleatória;
+Criar opções de menu adicionais para permitir que o usuário adicione ou remova frases do arquivo de texto;
+Criar uma opção de menu pro usuário pesquisar por uma palavra(?) (tinha pensado em ID mas ID so se fosse como dicionario) e exibir uma frase  que a possui;
+Implementar tratamento de erros para exceções como quando o arquivo de texto não existir ou não puder ser aberto.
+*/
--- PhraseGenerator/main.swift
+++ PhraseGenerator/main.swift
@@ -7,5 +7,79 @@
 
 import Foundation
 
-print("Hello, World!")
+enum MenuOption: Int, CaseIterable {
+case generateRandomPhrase = 1
+case addNewPhrase, removePhrase, showAllPhrases, searchPhraseByKeyword, quit
+    
+    var description: String {
+        switch self {
+        case .generateRandomPhrase:
+            return "Gerar nova frase aleatória"
+        case .addNewPhrase:
+            return "Adicionar nova frase"
+        case .removePhrase:
+            return "Remover frase existente"
+        case .showAllPhrases:
+            return "Exibir lista de frases"
+        case .searchPhraseByKeyword:
+            return "Pesquisar frase por palavra-chave"
+        case .quit:
+            return "Sair"
+        }
+    }
+} //:EOF Enum
 
+var shouldQuit = false
+
+func generateRandomPhrase() {}
+func addNewPhrase(_ phrase: String) {}
+func removePhrase(_ phrase: String) {}
+func getAllPhrases() -> [String] { return [] }
+
+func searchPhraseByKeyword(_ phrase: String) -> [String] { return [] }
+
+while (!shouldQuit) {
+    print("Selecione uma opção:")
+    for option in MenuOption.allCases {
+        print("\(option.rawValue). \(option.description)")
+    }
+    
+    
+    if let input = readLine(),
+       let selectedOption = Int(input),
+       let option = MenuOption(rawValue: selectedOption) {
+        switch option {
+        case .generateRandomPhrase:
+            let randomPhrase: () = generateRandomPhrase()
+            print(randomPhrase)
+        case .addNewPhrase:
+            
+            print("Digite a nova frase:")
+            if let newPhrase = readLine() {
+                addNewPhrase(newPhrase)
+                print("Nova frase adicionada com sucesso!")
+            }
+        case .removePhrase:
+            print("Digite a frase a ser removida:")
+            if let phraseToRemove = readLine() {
+                removePhrase(phraseToRemove)
+            }
+        case .showAllPhrases:
+            let phrases = getAllPhrases()
+            print("Lista de frases:")
+            for phrase in phrases {
+                print("- \(phrase)")
+            }
+        case .searchPhraseByKeyword:
+            print("Digite a palavra-chave:")
+            if let keyword = readLine() {
+                let matchingKeyword = searchPhraseByKeyword(keyword)
+                for phrase in matchingKeyword {
+                    print("Frases que contem /\(phrase)/")
+                }
+            }
+        case .quit:
+            break
+        }
+    }
+}
