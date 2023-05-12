a24ca8d4350582bab54655b2411ac73d1d2d4bc6
Noyalle Pereira <alley.pereira@hp.com>
2023-05-11 23:32:08 -0300

    wip: created remove func on PhrasesRequest class

--- PhraseGenerator/PhrasesRequest.swift
+++ PhraseGenerator/PhrasesRequest.swift
@@ -8,23 +8,38 @@
 import Foundation
 
 struct Frase: Codable {
-    let frases: [String]
+  let frases: [String]
 }
 
 class PhrasesRequest {
-    
-     func getting(completion: @escaping (_ phrase: [String]) -> Void) {
-        let _: URLSession = URLSession.shared
-        let readDocumentsURL = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!
-        let readFileURL = readDocumentsURL.appendingPathComponent("phrases.json")
-        do {
-            let data = try Data(contentsOf: readFileURL)
-            let fraseData = try JSONDecoder().decode(Frase.self, from: data)
-            completion(fraseData.frases)
-        } catch {
-            print(error)
-            completion([])
-        }
+  
+  func get(completion: @escaping (_ phrase: [String]) -> Void) {
+    let _: URLSession = URLSession.shared
+    let readDocumentsURL = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!
+    let readFileURL = readDocumentsURL.appendingPathComponent("phrases.json")
+    do {
+      let data = try Data(contentsOf: readFileURL)
+      let fraseData = try JSONDecoder().decode(Frase.self, from: data)
+      completion(fraseData.frases)
+    } catch {
+      print(error)
+      completion([])
     }
-    
+  }
+  
+  func remove(_ phrase: String) {
+    get { phrases in
+      if phrases.contains(phrase) {
+        let newPhrases = phrases.filter { $0 != phrase }
+        let readDocumentsURL = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!
+        let writeFileURL = readDocumentsURL.appendingPathComponent("phrases.json")
+        let newFraseData = Frase(frases: newPhrases)
+        let newFraseJSON = try! JSONEncoder().encode(newFraseData)
+        try! newFraseJSON.write(to: writeFileURL, options: .atomic)
+      } else {
+        print("A frase não foi encontrada no arquivo JSON.\n")
+      }
+    }
+  }
+  
 }
--- PhraseGenerator/main.swift
+++ PhraseGenerator/main.swift
@@ -8,25 +8,25 @@
 import Foundation
 
 enum MenuOption: Int, CaseIterable {
-    case generateRandomPhrase = 1
-    case addNewPhrase, removePhrase, showAllPhrases, searchPhraseByKeyword, quit
-    
-    var description: String {
-        switch self {
-        case .generateRandomPhrase:
-            return "Gerar nova frase aleatória"
-        case .addNewPhrase:
-            return "Adicionar nova frase"
-        case .removePhrase:
-            return "Remover frase existente"
-        case .showAllPhrases:
-            return "Exibir lista de frases"
-        case .searchPhraseByKeyword:
-            return "Pesquisar frase por palavra-chave"
-        case .quit:
-            return "Sair"
-        }
+  case generateRandomPhrase = 1
+  case addNewPhrase, removePhrase, showAllPhrases, searchPhraseByKeyword, quit
+  
+  var description: String {
+    switch self {
+    case .generateRandomPhrase:
+      return "Gerar nova frase aleatória"
+    case .addNewPhrase:
+      return "Adicionar nova frase"
+    case .removePhrase:
+      return "Remover frase existente"
+    case .showAllPhrases:
+      return "Exibir lista de frases"
+    case .searchPhraseByKeyword:
+      return "Pesquisar frase por palavra-chave"
+    case .quit:
+      return "Sair"
     }
+  }
 } //:EOF Enum
 
 var shouldQuit = false
@@ -34,52 +34,61 @@ let gettingRequest = PhrasesRequest()
 
 func generateRandomPhrase() {}
 func addNewPhrase(_ phrase: String) {}
-func removePhrase(_ phrase: String) {}
+func removePhrase(_ phrase: String) {
+  
+}
 
 func searchPhraseByKeyword(_ phrase: String) -> [String] { return [] }
 
 while (!shouldQuit) {
-    print("Selecione uma opção:")
-    for option in MenuOption.allCases {
-        print("\(option.rawValue). \(option.description)")
-    }
-    
-    if let input = readLine(),
-       let selectedOption = Int(input),
-       let option = MenuOption(rawValue: selectedOption) {
-        switch option {
-        case .generateRandomPhrase:
-            gettingRequest.getting { phrase in
-                print("- \(String(describing: phrase.randomElement()))\n")
-            }
-        case .addNewPhrase:
-            
-            print("Digite a nova frase:\n")
-            if let newPhrase = readLine() {
-                addNewPhrase(newPhrase)
-                print("Nova frase adicionada com sucesso!\n")
-            }
-        case .removePhrase:
-            print("Digite a frase a ser removida:\n")
-            if let phraseToRemove = readLine() {
-                removePhrase(phraseToRemove)
-            }
-        case .showAllPhrases:
-            gettingRequest.getting { phrase in
-                print("- \(phrase)\n")
-            }
-        case .searchPhraseByKeyword:
-            print("Digite a palavra-chave:\n")
-            if let keyword = readLine() {
-                let matchingKeyword = searchPhraseByKeyword(keyword)
-                for phrase in matchingKeyword {
-                    print("Frases que contem /\(phrase)/\n")
-                }
-            }
-        case .quit:
-            shouldQuit = true
+  print("Selecione uma opção:")
+  for option in MenuOption.allCases {
+    print("\(option.rawValue). \(option.description)")
+  }
+  
+  if let input = readLine(),
+     let selectedOption = Int(input),
+     let option = MenuOption(rawValue: selectedOption) {
+    switch option {
+    case .generateRandomPhrase:
+      gettingRequest.get { phrase in
+        print("- \(String(describing: phrase.randomElement()))\n")
+      }
+    case .addNewPhrase:
+      print("Digite a nova frase:\n")
+      if let newPhrase = readLine() {
+        addNewPhrase(newPhrase)
+        print("Nova frase adicionada com sucesso!\n")
+      }
+    case .removePhrase:
+      print("Digite a frase a ser removida:\n")
+      if let phraseToRemove = readLine() {
+        gettingRequest.get { phrase in
+          if phrase.contains(phraseToRemove) {
+            gettingRequest.remove(phraseToRemove)
+            print("Frase removida com sucesso:\n")
+            print("- \(phraseToRemove)\n")
+          } else {
+            print("A frase não foi encontrada no arquivo JSON.\n")
+          }
         }
-    } else {
-        print("Opção invalida\n")
+      }
+    case .showAllPhrases:
+      gettingRequest.get { phrase in
+        print("- \(phrase)\n")
+      }
+    case .searchPhraseByKeyword:
+      print("Digite a palavra-chave:\n")
+      if let keyword = readLine() {
+        let matchingKeyword = searchPhraseByKeyword(keyword)
+        for phrase in matchingKeyword {
+          print("Frases que contem /\(phrase)/\n")
+        }
+      }
+    case .quit:
+      shouldQuit = true
     }
+  } else {
+    print("Opção invalida\n")
+  }
 }
